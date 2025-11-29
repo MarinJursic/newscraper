c40b7d95fb22731e756f881b1a595ad3547ef7e1
Antonio <brkic123antonio@gmail.com>
2025-11-29 15:21:50 +0100

    fixed sidebar

---
+++ texy/src/components/layout/sidebar/Sidebar.tsx
@@ -0,0 +1,31 @@
+// AppSidebar.tsx
+"use client"
+
+import {
+    Sidebar,
+    SidebarHeader,
+    SidebarContent,
+    SidebarFooter,
+} from "@/components/ui/sidebar"
+
+import { Header } from "./components/SidebarHeader"
+import { SidebarMenuList } from "./components/MenuList"
+import { SidebarNotification } from "./components/Updates"
+import { UserControls } from "./components/UserControls"
+
+export function AppSidebar() {
+    return (
+        <Sidebar collapsible="icon">
+            <SidebarHeader />
+
+            <SidebarContent>
+                <SidebarMenuList />
+            </SidebarContent>
+
+            <SidebarFooter>
+                <SidebarNotification />
+                <UserControls />
+            </SidebarFooter>
+        </Sidebar>
+    )
+}
