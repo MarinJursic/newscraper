705379be11b86d42cd2f22bb1eeaef536437a9e3
OffCrazyFreak <jakovbla4@gmail.com>
2025-11-29 17:02:06 +0100

    fix

--- texy/src/app/layout.tsx
+++ texy/src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.variable} font-sans antialiased`}>
-        <div className="ml-8">{children}</div>
+        <div className="">{children}</div>
       </body>
     </html>
   );
